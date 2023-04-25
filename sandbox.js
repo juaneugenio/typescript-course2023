bcf9c058f4aac6c3f5af816b69e4a3920d6fc090
juan eugenio <leuviah77@gmail.com>
2023-04-25 22:08:59 +0200

    finished lesson 2 on branch lesson-2

---
+++ sandbox.js
@@ -0,0 +1,5 @@
+/** @format */
+var name = "Juan";
+console.log("👉 Line-2 ▶︎▶︎", name);
+var inputs = document.querySelectorAll("input");
+console.log("%c 👉 Line-5", "font-size:13px; background:#993441; color:#ffb8b1;", inputs);
