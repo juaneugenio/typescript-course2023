02c31bf9f442da1a6e13529cd73dab2ba01b3a8d
juan eugenio <leuviah77@gmail.com>
2023-04-27 20:25:34 +0200

    lesson-4: Objects and Array definitions

--- sandbox.ts
+++ sandbox.ts
@@ -1,16 +1,39 @@
 /** @format */
 
-const character = "juan";
-// character = "anna";
+// arrays
+let names = ["luigi", "mario", "yoshi"];
 
-let age = 30;
-// age = treinta;
+names.push("toad");
+// names.push(3);
+// names[1] = 3;
 
-let isBoolean = false;
-// isBoolean = "true";
+let numbers = [10, 20, 12, 15];
 
-const circleRad = (diameter: number) => {
-	return diameter * Math.PI;
+numbers.push(25);
+// numbers.push('shaun');
+// numbers[0] = 'shaun';
+
+let mixed = ["ken", 4, "chun-li", 8, 9];
+
+mixed.push("ryu");
+mixed.push(10);
+mixed[0] = 3;
+
+// objects
+let ninja = {
+	name: "mario",
+	belt: "black",
+	age: 30,
 };
-console.log("👉 Line-15 ▶︎▶︎", circleRad(12));
-console.log("👉 Line-15 ▶︎▶︎", circleRad(6));
+
+ninja.age = 40;
+ninja.name = "ryu";
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking']
+
+ninja = {
+	name: "yoshi",
+	belt: "orange",
+	age: 40,
+	// skills: ['running'],
+};
